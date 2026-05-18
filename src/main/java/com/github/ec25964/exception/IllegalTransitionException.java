7d511c2deccbf8746be4ff74cb8b77404dec3643
Matthew Hopkins <mattjhop@amazon.co.uk>
2026-05-18 14:50:05 +0100

    Add domain-specific exception hierarchy

---
+++ src/main/java/com/github/ec25964/exception/IllegalTransitionException.java
@@ -0,0 +1,8 @@
+package com.github.ec25964.exception;
+
+public class IllegalTransitionException extends DigitalIdException {
+
+    public IllegalTransitionException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/github/ec25964/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.github.ec25964.exception;
+
+public class NotFoundException extends DigitalIdException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
